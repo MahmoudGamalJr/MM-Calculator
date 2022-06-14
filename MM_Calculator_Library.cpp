38a222efddd1098787012a9a4eef8164b0bb8331
MAHMOUD GAMAL AHMED HASSANEIN <90795661+mahmoud0gamal0@users.noreply.github.com>
2022-06-14 16:20:57 +0200

    Update MM_Calculator_Library.cpp

--- MM_Calculator_Library.cpp
+++ MM_Calculator_Library.cpp
@@ -1,6 +1,6 @@
 /**
 * Name : MM Caluclator
-* Programmer : Mahmoud Gamal
+* Programmer : Mahmoud Gamal & Mostafa Gamal
 * Date :13.03.2020
 * For : Luxor Schools Students Technology Competition (All levels and NOT individual)
 **/
