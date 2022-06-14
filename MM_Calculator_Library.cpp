fddbc96a83e3848b07439e5e82abb497095a7b0d
MAHMOUD GAMAL AHMED HASSANEIN <90795661+mahmoud0gamal0@users.noreply.github.com>
2022-06-14 11:31:41 +0200

    Create MM_Calculator_Library.cpp

---
+++ MM_Calculator_Library.cpp
@@ -0,0 +1,34 @@
+/**
+* Name : MM Caluclator
+* Programmer : Mahmoud Gamal,Mostafa Gamal
+* Date :13.03.2020
+* For : Luxor Students Competition (All levels and NOT individual)
+**/
+#ifndef MAIN_H
+	#define MAIN_H
+
+	#include <iostream>
+	#include <cmath>
+	#include <inttypes.h>
+	
+	using namespace std;
+
+	void 	Addition();
+	void 	Subtraction();
+	void 	Division();
+	void 	Multiplication();
+	void 	Exponentiation();
+	void 	SquareRoot();
+	void 	Sine();
+	void 	Cosine();
+	void 	Tangent();
+	void 	help();
+	void 	error();
+
+	float 	degree2Radian(float degree);
+	float 	implementationPower(float base, int power);
+	int64_t implementationFactorial(int number);
+	float 	implementationSine(float degree);
+	float 	implementationCosine(float degree);
+
+#endif
